dd5146d59b3110410923152203a9b5bbe255bf41
clovis <clovis.rodriguez@rappi.com>
2018-10-04 10:17:07 -0500

    Book description

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function(){
   return [
-    {title:'Javascript: the good parts'},
-    {title:'Harry Poter'},
-    {title:'The people'},
-    {title:'My good fellas'}
+    {title:'Javascript: the good parts', pages: 90},
+    {title:'Harry Poter', pages: 23},
+    {title:'The people', pages: 1002},
+    {title:'My good fellas', pages: 54}
   ]
 }
